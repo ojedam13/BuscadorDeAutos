de13ee64fa0bb4ff5b86985b012c40ff3fca20f8
ojedam13 <m_ojeda17@hotmail.com>
2021-08-25 16:20:51 -0300

    filtrar auto por marca

--- js/app.js
+++ js/app.js
@@ -39,6 +39,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 marca.addEventListener('change', e => {
     datosBusqueda.marca = e.target.value;
+
+    filtrarAuto()
 })
 
 year.addEventListener('change', e => {
@@ -92,4 +94,19 @@ function llenarSelect() {
         opcion.textContent = i;
         year.appendChild(opcion); //agrega las opciones del año a select
     }
+}
+
+//funcion que filtra en base a la busqueda
+function filtrarAuto() {
+    const resultado = autos.filter(filtrarMarca)
+   
+}
+
+function filtrarMarca(auto) {
+    const { marca } = datosBusqueda;
+    if (marca) {
+        return auto.marca === marca;
+    }
+
+    return auto;
 }
